74576a942c0edf8e49b8a80e3dd44c5228eb181b
Gabriel Isaias Gonzalez <ggonzalez@ciidept.local>
2021-11-17 12:18:37 -0300

    agregué css a cada html

--- catalogo.js
+++ catalogo.js
@@ -1,18 +1,18 @@
 window.addEventListener('DOMContentLoaded',()=>{
-    const orderButton = document.querySelectorAll('button[data-order]');
+    const orderButton = document.querySelectorAll('button[data-order]'); //query te da un trozo de html
     
     orderButton.forEach((item) => {
         item.addEventListener('click',(event)=>{
             const button = event.currentTarget;
-            const container = button.parentNode
-            let order = {
-                title: container.querySelector('.title').innerText,
+            const container = button.parentNode; //parentNode me devuelve el html padre
+            const order = {
+                title: container.querySelector('.title').innerText, //inner te da el texto encerrado en la etiqueta
                 price: container.querySelector('.price').innerText,
-                id: button.getAttribute('data-order')
+                gabriel: button.getAttribute('data-order')
             }
-            localStorage.setItem('order', JSON.stringify(order));
-            const url = window.location.href.replace('catalogo.html', 'order.html');
-            window.location.href = url;
+            localStorage.setItem('order', JSON.stringify(order)); //JSON.str... me stringuea el objeto order
+            //window:ventana donde toy -- location: ubicacion--href es el link EJ:https://translate.google.com/?sl=en&tl=es&text=parse&op=translate
+            window.location.href = "order.html"; //esto me manda directo a donde quiero
         });
     })
 });
